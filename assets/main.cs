92b639f357da22e51b8de068564565bdebfcce06
Crafterbot <71361713+TheOmag@users.noreply.github.com>
2021-10-23 23:13:21 -0400

    Add files via upload

---
+++ assets/main.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace TPkarlson
+{
+    [HarmonyPatch(typeof(PlayerMovement))]
+    [HarmonyPatch("Update", MethodType.Normal)]
+
+    public class code
+    {
+        private static bool hitOneOff = false;
+
+        private static void Postfix(PlayerMovement __instance)
+        {
+            if (Input.GetKey(KeyCode.T) && !hitOneOff)
+            {
+                Ray mainRay = new Ray(__instance.rb.position + new Vector3(0f, 0.6f, 0f), __instance.playerCam.transform.forward);
+                Physics.Raycast(mainRay, 10000);
+                RaycastHit hit;
+                if (Physics.Raycast(mainRay, out hit))
+                {
+                    __instance.rb.transform.position = hit.point + new Vector3(0f, 0.4f, 0f);
+                }
+                hitOneOff = true;
+            }
+            if(!Input.GetKey(KeyCode.T))
+            {
+                hitOneOff = false;
+            }
+        }
+    }
+}
+/*
+ resouces
+
+tp code
+__instance.rb.position = new Vector3(0f, 0f, 0f);
+
+ 
+ */
---
+++ assets/patcher.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Reflection;
+
+namespace TPkarlson
+{
+
+    [BepInPlugin("org.Crafterbot.karlson.TPkarlson", "TP karlson", "5.4.17.0")]
+    public class MyPatcher : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            var harmony = new Harmony("com.Crafterbot.karlsonMod.TPkarlson");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
